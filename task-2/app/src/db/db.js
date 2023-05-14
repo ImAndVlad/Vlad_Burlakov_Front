8848eb56d367e693681a21a406de6e33134112b8
Vlad Burlakov <vladbrlkv@gmail.com>
2023-05-15 02:50:23 +0300

    added data

---
+++ task-2/app/src/db/db.js
@@ -0,0 +1,28 @@
+import oneImg from "../image/1.jpg"
+import twoImg from "../image/2.jpg"
+import threeImg from "../image/3.jpg"
+import fourImg from "../image/4.jpg"
+import fiveImg from "../image/5.jpg"
+import sixImg from "../image/6.jpg"
+import sevenImg from "../image/7.jpg"
+import eightImg from "../image/8.jpg"
+import nineImg from "../image/9.jpg"
+import tenImg from "../image/10.jpg"
+import elevenImg from "../image/11.jpg"
+import twelveImg from "../image/12.jpg"
+export function getData() {
+    return [
+        {Image: oneImg},
+        {Image: twelveImg},
+        {Image: threeImg},
+        {Image: fourImg},
+        {Image: fiveImg},
+        {Image: sixImg},
+        {Image: sevenImg},
+        {Image: eightImg},
+        {Image: nineImg},
+        {Image: tenImg},
+        {Image: elevenImg},
+        {Image: twelveImg},
+    ];
+}
