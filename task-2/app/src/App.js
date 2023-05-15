494eb527e314985daa28f12ba7523596511660b4
Vlad Burlakov <vladbrlkv@gmail.com>
2023-05-15 04:34:37 +0300

    photo

--- task-2/app/src/App.js
+++ task-2/app/src/App.js
@@ -9,10 +9,12 @@ function App() {
     return (
         <>
             <h1 className="heading">Photos</h1>
-            <div className="cards__container">
-                {photos.map((photo) => {
-                    return <Card photo={photo} />;
-                })}
+            <div className="three-col-grid">
+                <div className="cards__container">
+                    {photos.map((photo) => {
+                        return <Card photo={photo}/>;
+                    })}
+                </div>
             </div>
         </>
     );
